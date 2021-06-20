8a5611f72807333b8e114764599a6a47a981485f
eihwan-sfdc <eihwan.kim@salesforce.com>
2021-06-20 14:58:56 +0900

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        config(['evergage.account' => 'ekim1482497']);
+        config(['evergage.dataset' => 'engage']);
     }
 }
