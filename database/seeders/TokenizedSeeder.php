bf3b3c15dc268854924a5b8b213045fb7b492437
eihwan-sfdc <eihwan.kim@salesforce.com>
2021-09-12 08:51:32 +0900

    .

--- database/seeders/TokenizedSeeder.php
+++ database/seeders/TokenizedSeeder.php
@@ -17,7 +17,6 @@ class TokenizedSeeder extends Seeder
         //
         DB::table('tokenized')->insert(
             [
-                'id' => '1',
                 'email_token' => 'abc@example.com',
                 'first_name' => 'Eihwan',
                 'last_name' => 'Kim',
@@ -25,7 +24,6 @@ class TokenizedSeeder extends Seeder
                 'subkey' => 'subkey001',
             ],
             [
-                'id' => '2',
                 'email_token' => 'def@example.com',
                 'first_name' => 'Taro',
                 'last_name' => 'Yamada',
@@ -33,7 +31,6 @@ class TokenizedSeeder extends Seeder
                 'subkey' => 'subkey002',
             ],
             [
-                'id' => '3',
                 'email_token' => 'hij@example.com',
                 'first_name' => 'Taro2',
                 'last_name' => 'Yamada2',
