99ca8aee06c27b419519d52867a8ba6f5a1f21e1
eihwan-sfdc <eihwan.kim@salesforce.com>
2021-09-12 09:36:16 +0900

    .

--- app/Http/Controllers/TokenizedSendingController.php
+++ app/Http/Controllers/TokenizedSendingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class TokenizedSendingController extends Controller
 {
@@ -31,8 +32,26 @@ class TokenizedSendingController extends Controller
         $token = $tokens[0]['token'];
         Log::emergency($token);
         
+        $items = DB::table('tokenized')
+                    ->select('*')
+                    ->where('email_token', $token)
+                    ->get();
 
-        
+        $resultArray = array();
+        if ($items) {
+            $resultArray['resolvedTokens'] = [];
+            $resultArray['resolvedTokens']['tokenRequestId'] = $token;
+            $resultArray['resolvedTokens']['tokenValue'] = $items[0]['subkey'];
+        } else {
+            $resultArray['unresolvedTokens'] = [];
+            $resultArray['unresolvedTokens']['tokenRequestId'] = $token;
+            $resultArray['unresolvedTokens']['message'] = 'Invalid token; token does not exist.' ;
+        }
+
+        $response = json_encode($resultArray);
+        Log::emergency(json_encode($resultArray));
+        echo $response;
+
     }
 
     public function gettest() {
