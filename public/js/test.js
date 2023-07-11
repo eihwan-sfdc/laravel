6d443a60fb1a161b3727f0554d7ca55843ab2e9f
eihwan-sfdc <eihwan.kim@salesforce.com>
2023-07-11 17:24:59 +0900

    added file for lesencrypt auth

--- public/js/test.js
+++ public/js/test.js
@@ -1,1 +1,21 @@
-const testeihwan = 'hoge';
+[{"to":"U0da2bef0177a28c4539abf3cdf8c9850",
+"messages":[
+  {"type":"template",
+  "altText":"??5??10%???????????????????",
+  "template":{
+    "type":"carousel",
+    "imageAspectRatio":"rectangle",
+    "imageSize":"cover",
+    "actions":[],
+    "columns":[
+      {
+        "thumbnailImageUrl":"https://image.charleskeith.jp/lib/fe3211737164047f771277/m/1/c16d7757-553d-49b7-9f0c-c1b3e17d4438.jpg",
+        "imageBackgroundColor":"#FFFFFF",
+        "title":"????????5?",
+        "text":"?????????5?25?(?)?????????????????????",
+        "actions":[{
+          "type":"uri",
+          "label":"?????????????",
+          "uri":"https://charleskeith.jp/mypage/coupon-list?utm_source=LINE&utm_medium=LINE&utm_campaign=SS23_VIPcoupon&utm_term=20230526_20230526&utm_content=card_VIPcoupon"
+          }]
+      },{"thumbnailImageUrl":"https://image.charleskeith.jp/lib/fe3211737164047f771277/m/1/0bffb371-398d-4f0c-b4ac-60c803777233.jpg","imageBackgroundColor":"#FFFFFF","title":"????????????","text":"?????????????????????????????????????????????","actions":[{"type":"uri","label":"??????","uri":"https://charleskeith.jp/category/sandal_l_plat?commercialType=0%7C2%7C3&currentPage=1&alignmentSequence=news+from+date%28text%29%3Adescending&utm_source=LINE&utm_medium=LINE&utm_campaign=SS23_VIPcoupon&utm_term=20230526_20230526&utm_content=card_shoes-platform"}]},{"thumbnailImageUrl":"https://image.charleskeith.jp/lib/fe3211737164047f771277/m/1/4412a3d1-0843-4226-9e21-f5de9a78b67c.jpg","imageBackgroundColor":"#FFFFFF","title":"????????","text":"?????????????????????????????????????????????","actions":[{"type":"uri","label":"??????","uri":"https://charleskeith.jp/category/aa11650?commercialType=0%7C2%7C3&currentPage=1&alignmentSequence=news+from+date%28text%29%3Adescending&utm_source=LINE&utm_medium=LINE&utm_campaign=SS23_VIPcoupon&utm_term=20230526_20230526&utm_content=card_shoes-mule"}]},{"thumbnailImageUrl":"https://image.charleskeith.jp/lib/fe3211737164047f771277/m/1/04ec61e3-35b3-4eae-ae3f-6b6a25b330bb.jpg","imageBackgroundColor":"#FFFFFF","title":"????????","text":"???????????????????????????????????????","actions":[{"type":"uri","label":"??????","uri":"https://charleskeith.jp/category/ladies_bags?commercialType=0%7C2%7C3&searchWord=%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%90%E3%82%B9&currentPage=1&alignmentSequence=news+from+date%28text%29%3Adescending&utm_source=LINE&utm_medium=LINE&utm_campaign=SS23_VIPcoupon&utm_term=20230526_20230526&utm_content=card_bag-canvas"}]},{"thumbnailImageUrl":"https://image.charleskeith.jp/lib/fe3211737164047f771277/m/1/189411dc-2a1e-472d-94b1-05e594e45a57.jpg","imageBackgroundColor":"#FFFFFF","title":"???????","text":"?????????????????????????????????????","actions":[{"type":"uri","label":"??????","uri":"https://charleskeith.jp/category/ladies_bags?commercialType=0%7C1%7C2%7C3&searchColor=%E3%82%B0%E3%83%AA%E3%83%BC%E3%83%B3%7C%E3%83%96%E3%83%AB%E3%83%BC%7C%E3%83%91%E3%83%BC%E3%83%97%E3%83%AB%7C%E3%82%A4%E3%82%A8%E3%83%AD%E3%83%BC%7C%E3%82%AA%E3%83%AC%E3%"}]},{"thumbnailImageUrl":"https://image.charleskeith.jp/lib/fe3211737164047f771277/m/1/0ec2a565-1a87-4b2f-88f0-8b3f8a42567e.png","imageBackgroundColor":"#FFFFFF","title":"??????????","text":"????\xB7???????","actions":[{"type":"uri","label":"??????","uri":"https://charleskeith.jp/mypage/coupon-list?utm_source=LINE&utm_medium=LINE&utm_campaign=SS23_VIPcoupon&utm_term=20230526_20230526&utm_content=card_VIPcoupon"}]}],"config":{"image":"on","title":"on"}}}]}]
--- public/test.sql
+++ public/test.sql
@@ -1,78 +1,32 @@
-SELECT  socc.[SubscriberKey]                                                                                  AS 'ContactKey'
-       ,socc.[JobId]                                                                                          AS 'JobId'
-       ,socc.[Domain]                                                                                         AS 'Domain'
-       ,socc.[ListID]                                                                                         AS 'ListID'
-       ,socc.[BatchID]                                                                                        AS 'BatchID'
-       ,'TRUE'                                                                                                AS 'Sent'
-       ,j.[EmailName]                                                                                         AS 'MessageName'
-       ,j.[EmailSubject]                                                                                      AS 'Subject'
-       ,j.[EmailID]                                                                                           AS 'AssetId'
-       ,socc.[EmailSentDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'GMT Standard Time'    AS 'SentDate'
-       ,(CASE WHEN socc.[EmailOpenUnique] IS NULL THEN 'FALSE' ELSE 'TRUE' END )                              AS 'Opened'
-       ,socc.[EmailOpenDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'GMT Standard Time'    AS 'OpenedDate'
-       ,(CASE WHEN socc.[EmailClickUnique] IS NULL THEN 'FALSE' ELSE 'TRUE' END )                             AS 'Clicked'
-       ,socc.[EmailClickDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'GMT Standard Time'   AS 'ClickedDate'
-       ,(CASE WHEN socc.[Unsubscribed] IS NULL THEN 'FALSE' ELSE 'TRUE' END)                                  AS 'Unsubscribed'
-       ,socc.[UnsubscribedDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'GMT Standard Time' AS 'UnsubscribedDate'
-       ,(CASE WHEN socc.[Bounced] IS NULL THEN 'FALSE' ELSE 'TRUE' END)                                       AS 'Bounced'
-       ,socc.[BouncedDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'GMT Standard Time'      AS 'BouncedDate'
-       ,socc.[BounceType]                                                                                     AS 'BouncedType'
-       ,socc.[BounceDetail]                                                                                   AS 'BouncedDetail'
-       ,socc.[Delivered]                                                                                      AS 'Delivered'
-       ,(CASE WHEN socc.[Delivered] = 'False' THEN 'TRUE' ELSE 'FALSE' END)                                   AS 'Undelivered'
-       ,JR.JourneyName                                                                                        AS 'JourneyName'
-       ,JR.JourneyID                                                                                          AS 'JourneyID'
-       ,JR.VersionNumber                                                                                      AS 'JourneyVersion'
-       ,JA.ActivityID                                                                                         AS 'ActivityID'
-       ,JA.ActivityName                                                                                       AS 'ActivityName'
-       ,GETDATE() AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'GMT Standard Time'               AS 'LastModifiedDate'
-       ,JR.VersionID                                                                                          AS 'JourneyVersionID'
-       ,'Email'                                                                                               AS 'ContactChannel'
-       ,S.EmailAddress                                                                                        AS 'Email'
-       ,(CASE WHEN SUBSTRING(REPLACE(JR.JourneyName,' ',''),12,4) = 'Surv' OR SUBSTRING(REPLACE(JR.JourneyName,' ',''),12,4) = 'Noti' THEN SUBSTRING(REPLACE(JR.JourneyName,' ',''),12,6) ELSE SUBSTRING(REPLACE(JR.JourneyName,' ',''),0,7) END) AS 'CampaignCode'
-       ,(CASE WHEN ISNUMERIC(SUBSTRING(REPLACE(JR.JourneyName,' ',''),0,7)) = 1 THEN 'Campaign' WHEN SUBSTRING(REPLACE(JR.JourneyName,' ',''),12,6) = 'Survey' THEN 'Survey' WHEN SUBSTRING(REPLACE(JR.JourneyName,' ',''),12,4) = 'Noti' THEN 'Notification' ELSE NULL END ) AS 'ContactType'
+SELECT  t1.ORDER_NO
+       ,t1.MODEL_ID
+       ,t1.MODEL_NAME
+       ,t1.SELLING_UNIT_PRICE
+       ,t1.ITEM_CHARACTER_GROUP_R3_CHARACTER_SERIES_CD
+       ,t1.AUTO_RETENTION_COND_MASTER_CATEGORY_NO
+       ,t1.MODEL_CATEGORY_MASTER_CATEGORY_NO_KBN
+       ,t1.ITEM_MASTER_ITEM_CD
+       ,t1.PRODUCT_TYPE
+       ,t2.ORDER_NO               AS 'RELATED_ORDER_NO'
+       ,t2.MODEL_ID               AS 'RELATED_MODEL_ID'
+       ,t2.CATEGORY_NO            AS 'RELATED_CATEGORY_NO'
+       ,t2.CATEGORY_NO_KBN        AS 'RELATED_CATEGORY_NO_KBN'
+       ,t2.ITEM_CD                AS 'RELATED_ITEM_CD'
+       ,t2.R3_CHARACTER_SERIES_CD AS 'RELATED_R3_CHARACTER_SERIES_CD'
 FROM
 (
-	SELECT  ROW_NUMBER() OVER (PARTITION BY s.SubscriberKey,s.JobID ORDER BY s.BatchID DESC) AS Row_Num
-	       ,s.AccountID
-	       ,s.OYBAccountID
-	       ,s.JobID
-	       ,s.ListID
-	       ,s.BatchID                                                                        AS 'BatchID'
-	       ,s.SubscriberID                                                                   AS 'SubscriberID'
-	       ,s.SubscriberKey
-	       ,s.EventDate                                                                      AS 'EmailSentDate'
-	       ,s.Domain
-	       ,s.TriggererSendDefinitionObjectID
-	       ,s.TriggeredSendCustomerKey
-	       ,o.EventDate                                                                      AS 'EmailOpenDate'
-	       ,o.IsUnique                                                                       AS 'EmailOpenUnique'
-	       ,c.EventDate                                                                      AS 'EmailClickDate'
-	       ,c.IsUnique                                                                       AS 'EmailClickUnique'
-	       ,u.[IsUnique]                                                                     AS 'Unsubscribed'
-	       ,u.[EventDate]                                                                    AS 'UnsubscribedDate'
-	       ,b.[BounceCategory]                                                               AS 'BounceType'
-	       ,b.[BounceSubcategory]                                                            AS 'BounceDetail'
-	       ,b.[IsUnique]                                                                     AS 'Bounced'
-	       ,b.[EventDate]                                                                    AS 'BouncedDate'
-	       ,(CASE WHEN b.[IsUnique] IS NOT NULL THEN 'FALSE' ELSE 'TRUE' END)                AS 'Delivered'
-	FROM [_Sent] s
-	FULL OUTER JOIN [_Open] o
-	ON s.[JobID] = o.[JobID] AND s.[BatchID] = o.[BatchID] AND s.[SubscriberKey] = o.[SubscriberKey] AND o.[IsUnique] = 1
-	FULL OUTER JOIN [_Click] c
-	ON s.[JobID] = c.[JobID] AND s.[BatchID] = c.[BatchID] AND s.[SubscriberKey] = c.[SubscriberKey] AND c.[IsUnique] = 1
-	FULL OUTER JOIN [_Unsubscribe] u
-	ON s.[JobID] = u.[JobID] AND s.[BatchID] = u.[BatchID] AND s.[SubscriberKey] = u.[SubscriberKey] AND u.[IsUnique] = 1
-	FULL OUTER JOIN [_Bounce] b
-	ON s.[JobID] = b.[JobID] AND s.[BatchID] = b.[BatchID] AND s.[SubscriberKey] = b.[SubscriberKey] AND b.[IsUnique] = 1
-) AS socc
-JOIN [_Job] j
-ON socc.[JobID] = j.[JobID]
-JOIN [_JourneyActivity] JA
-ON socc.[TriggererSendDefinitionObjectID] = JA.[JourneyActivityObjectID]
-JOIN [_Journey] JR
-ON JA.[VersionID] = JR.[VersionID]
-JOIN [_Subscribers] S
-ON socc.[SubscriberID] = S.[SubscriberID]
-WHERE LEN(socc.[SubscriberKey]) = 18 /*AND socc.[EmailSentDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time' >= '2023-06-01 00:00:00'*/
-AND ((socc.[EmailSentDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time') >= DATEADD(hour, -6, CONVERT(DATETIME, CONVERT(DATE, DATEADD(day, -1, GETDATE() AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time')))) OR (socc.[EmailOpenDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time') >= DATEADD(hour, -6, CONVERT(DATETIME, CONVERT(DATE, DATEADD(day, -1, GETDATE() AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time')))) OR (socc.[EmailClickDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time') >= DATEADD(hour, -6, CONVERT(DATETIME, CONVERT(DATE, DATEADD(day, -1, GETDATE() AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time')))) OR (socc.[UnsubscribedDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time') >= DATEADD(hour, -6, CONVERT(DATETIME, CONVERT(DATE, DATEADD(day, -1, GETDATE() AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time')))) OR (socc.[BouncedDate] AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time') >= DATEADD(hour, -6, CONVERT(DATETIME, CONVERT(DATE, DATEADD(day, -1, GETDATE() AT TIME ZONE 'Central America Standard Time' AT TIME ZONE 'West Pacific Standard Time')))) )
+	SELECT  ORDER_NO
+	       ,MODEL_ID
+	       ,MODEL_NAME
+	       ,SELLING_UNIT_PRICE
+	       ,ITEM_CHARACTER_GROUP_R3_CHARACTER_SERIES_CD
+	       ,AUTO_RETENTION_COND_MASTER_CATEGORY_NO
+	       ,MODEL_CATEGORY_MASTER_CATEGORY_NO_KBN
+	       ,ITEM_MASTER_ITEM_CD
+	       ,PRODUCT_TYPE
+	FROM c514000640.[【改修】03_SENDABLE_PRODUCTS] with (nolock)
+	WHERE LEN(AUTO_RETENTION_COND_MASTER_CATEGORY_NO) = 4 
+) t1
+INNER JOIN c514000640.[【改修】04_RELATED_PRODUCTS_01_3] t2 with (nolock)/* 「ITEM_CHARACTER_GROUP」の「R3_CHARACTER_SERIES_CD」に、新着/締切商品抽出の際に取得した「ITEM_CHARACTER_GROUP」の「R3_CHARACTER_SERIES_CD」で絞込 */
+ON t1.ITEM_CHARACTER_GROUP_R3_CHARACTER_SERIES_CD = t2.R3_CHARACTER_SERIES_CD /* 「MODEL_CATEGORY_MASTER」の「CATEGORY_NO」に、新着/締切商品抽出の際に取得した「AUTO_RETENTION_COND_MASTER」の「CATEGORY_NO」で絞込 */ /* ⇒新着/締切商品の親カテゴリNo(長さが4)であれば子カテゴリNoにも一致とする */ AND t2.CATEGORY_NO LIKE t1.AUTO_RETENTION_COND_MASTER_CATEGORY_NO + '%' /* 「MODEL_CATEGORY_MASTER」の「CATEGORY_NO_KBN」に、新着/締切商品抽出の際に取得した「MODEL_CATEGORY_MASTER」の「CATEGORY_NO_KBN」で絞込(商品抽出の際に区分を'M'で指定しているため抽出された区分も'M') */ AND t1.MODEL_CATEGORY_MASTER_CATEGORY_NO_KBN = t2.CATEGORY_NO_KBN /* 「ITEM_MASTER」の「ITEM_CD」に、新着/締切商品抽出の際に取得した「ITEM_MASTER」の「ITEM_CD」で合致しないこと */ AND t1.ITEM_MASTER_ITEM_CD <> t2.ITEM_CD /* 「ORDER_NO_MASTER」の「MODEL_ID」に、新着/締切商品抽出の際に取得した「ORDER_NO_MASTER」の「MODEL_ID」で合致しないこと */ 
+AND t1.MODEL_ID <> t2.MODEL_ID
